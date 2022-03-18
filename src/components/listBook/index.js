018ac0ce6f94756458dfbae3c47739eb75210ea5
Kayro França <k4yro01@gmail.com>
2022-03-18 19:22:37 -0300

    updated component

--- src/components/listBook/index.js
+++ src/components/listBook/index.js
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
-import getBooks from "../../requestBook";
+import bookInfo from "../../modal/bookInfo";
+import getBooksFromApi from "../../requestBook";
 import CardBook from "../cardBook";
 import { ListBookContainer, Loading } from "./styles";
 
-const ListBook = () => {
+const ListBook = (props) => {
+    const [getBooks, setGetBooks] = useState([]);
     const [books, setBooks] = useState([]);
-    const [vI, setVI] = useState([]);
     useEffect(() => {
-        getBooks(setBooks);
-    }, []);
+        getBooksFromApi(setGetBooks, props.search);
+    }, [props.search]);
     useEffect(() => {
-        setVI(books.map(e => e.volumeInfo));
-    }, [books]);
+        setBooks(getBooks.map(e => e.volumeInfo));
+    }, [getBooks]);
     return (
         <ListBookContainer>
             {
-                vI.length === 0 ? 
+                books.length === 0 ? 
                     <Loading>loading... </Loading> 
-                    : vI.map(book =>  <CardBook book={book}/>)
+                    : books.map((book, i) =>  {
+                        const coverBook = book.imageLinks === undefined ? undefined : book.imageLinks.thumbnail;
+                        const bookMapped = new bookInfo(book.title, book.subtitle, book.authors,
+                                                coverBook, book.publishedDate, 
+                                                book.description, book.infoLink);
+                        return <CardBook key={i} book={bookMapped}/>
+                    })
             }
         </ListBookContainer>
         
