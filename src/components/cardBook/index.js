8d05786cc3d8f06e9a9e2d0c3c1c534b167613fb
Kayro França <k4yro01@gmail.com>
2022-03-18 19:19:50 -0300

    updated src from img and refactor text from Title

--- src/components/cardBook/index.js
+++ src/components/cardBook/index.js
@@ -3,9 +3,15 @@ import { Card, DescriptionText, Subtitle, Title, DescriptionContainer } from "./
 const CardBook = (props) => {
     return (
         <Card>
-            <img src={props.book.imageLinks.thumbnail} alt="img" />
+            <img src={props.book.imageBook} alt="img" />
             <DescriptionContainer>
-                <Title href={props.book.infoLink} target="_blank">{props.book.title}: {props.book.subtitle}</Title>
+                <Title href={props.book.infoLink} target="_blank">
+                    {
+                        props.book.subtitle === '' ?
+                            `${props.book.title}`
+                            : `${props.book.title}: ${props.book.subtitle}`
+                    }
+                </Title>
                 <p>
                     {props.book.authors.join(', ')} &#8226;
                     <Subtitle> {props.book.publishedDate.substr(0,4)}</Subtitle>
