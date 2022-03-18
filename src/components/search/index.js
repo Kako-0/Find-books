ae6f7c556b302c442f8eef042a5ca350e9bf20b6
Kayro França <k4yro01@gmail.com>
2022-03-18 19:24:25 -0300

    updated component

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
+import ListBook from "../listBook";
 import { BtnMain, ContainerMain, DivSearch, InputMain } from "./styles";
 
 const Search = () => {
+    const [toogle, setToogle] = useState(false);
+    const [inputSearch, setInputSearch] = useState('');
+    const [submitInputSearch, setSubmitInputSearch] = useState('');
+
+    const submitSearch = () => {
+        if (inputSearch.length < 2) return;
+        if (inputSearch.length !== 0 && toogle === true){
+            setSubmitInputSearch(inputSearch);
+        }else{
+            setSubmitInputSearch(inputSearch);
+            setToogle(!toogle);
+        }
+    }
+
     return (
-        <ContainerMain>
-            <DivSearch>
-                <InputMain type="text" placeholder="Digite sua busca (livro, autor...)"/>
-                <BtnMain type="button"/>
-            </DivSearch>
-        </ContainerMain>
+        <>
+            <ContainerMain>
+                <DivSearch>
+                    <InputMain 
+                        type="text" 
+                        value={inputSearch}
+                        onChange={(e) => setInputSearch(e.target.value)} 
+                        placeholder="Digite sua busca (livro, autor...)"
+                    />
+                    <BtnMain 
+                        type="button" 
+                        onClick={submitSearch}
+                    />
+                </DivSearch>
+            </ContainerMain>
+            {toogle && <ListBook search={submitInputSearch}/>}
+        </>
     );
 }
 
